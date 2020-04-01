26ef797987d0a11c48c7c3481188652d2a9e5977
Egor Ivkov <e.o.ivkov@gmail.com>
2020-04-01 13:24:18 +0300

    encapsulate stats addition
    also added more tests
    Signed-off-by: Egor Ivkov <e.o.ivkov@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,14 @@
 use chashmap::CHashMap;
-use std::{fmt, time::Duration, io::{self, prelude::*}, fs::File};
+use std::{
+    fmt,
+    fs::File,
+    io::{self, prelude::*},
+    time::Duration,
+};
 
 pub struct Stats {
-    pub pings_to_peers: CHashMap<String, Vec<Duration>>,
-    pub transmissions_rates: CHashMap<String, Vec<Duration>>,
+    pings_to_peers: CHashMap<String, Vec<Duration>>,
+    transmissions_rates: CHashMap<String, Vec<Duration>>,
     window_size: usize,
     peer_id: String,
 }
@@ -18,11 +23,58 @@ impl Stats {
         }
     }
 
-    pub fn save_to_file(&self, filename: &str) -> io::Result<()>{
+    pub fn save_to_file(&self, filename: &str) -> io::Result<()> {
         let mut file = File::create(filename)?;
         file.write_all(self.to_string().as_bytes())?;
         Ok(())
     }
+
+    pub fn add_ping(&self, peer_id: String, rtt: Duration) {
+        if !self.pings_to_peers.contains_key(&peer_id) {
+            self.pings_to_peers.insert_new(peer_id.clone(), Vec::new())
+        }
+        self.pings_to_peers
+            .get_mut(&peer_id)
+            .expect("Failed to get peer entry")
+            .push_lossy(rtt, self.window_size)
+    }
+
+    pub fn add_transmission(&self, peer_id: String, time: Duration, n_bytes: u32) {
+        if !self.transmissions_rates.contains_key(&peer_id) {
+            self.transmissions_rates
+                .insert_new(peer_id.clone(), Vec::new())
+        }
+        self.transmissions_rates
+            .get_mut(&peer_id)
+            .expect("Failed to get peer entry")
+            .push_lossy(
+                //put transmission rate which is elapsed time per byte
+                time / n_bytes,
+                self.window_size,
+            )
+    }
+}
+
+#[test]
+fn correctly_added_pings() {
+    let stats = Stats::new(100, "1".to_string());
+    stats.add_ping("2".to_string(), Duration::from_secs(1));
+    stats.add_ping("2".to_string(), Duration::from_secs(2));
+    stats.add_ping("3".to_string(), Duration::from_secs(1));
+    assert_eq!(stats.pings_to_peers.len(), 2);
+    let peer_2_pings = stats.pings_to_peers.get("2").unwrap();
+    assert_eq!(peer_2_pings.len(), 2)
+}
+
+#[test]
+fn correctly_added_transmissions() {
+    let stats = Stats::new(100, "1".to_string());
+    stats.add_transmission("2".to_string(), Duration::from_secs(1), 1);
+    stats.add_transmission("2".to_string(), Duration::from_secs(2), 1);
+    stats.add_transmission("3".to_string(), Duration::from_secs(1), 1);
+    assert_eq!(stats.transmissions_rates.len(), 2);
+    let peer_2_transmissions = stats.transmissions_rates.get("2").unwrap();
+    assert_eq!(peer_2_transmissions.len(), 2)
 }
 
 fn durations_mean(durations: &Vec<Duration>) -> Option<Duration> {
@@ -136,4 +188,13 @@ impl<T> PushLossy<T> for Vec<T> {
         }
         self.push(element);
     }
+}
+
+#[test]
+fn correct_push_lossy() {
+    let mut vector = Vec::new();
+    vector.push_lossy(1, 2);
+    vector.push_lossy(2, 2);
+    vector.push_lossy(3, 2);
+    assert_eq!(vector, vec![2, 3]);
 }
